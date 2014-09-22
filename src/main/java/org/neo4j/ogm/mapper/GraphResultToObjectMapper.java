68da67078148c9c14450c4fc235cf321ec900cf1
atg103 <atg103@outlook.com>
2014-09-22 19:36:27 +0100

    Adding initial packages/interfaces and graph-model dependency.

---
+++ src/main/java/org/neo4j/ogm/mapper/GraphResultToObjectMapper.java
@@ -0,0 +1,20 @@
+package org.neo4j.ogm.mapper;
+
+import org.graphaware.graphmodel.Graph;
+
+/**
+ * Specification for an object-graph mapper, which can map {@link Graph}s onto arbitrary Java objects.
+ *
+ * @param <T> The type of object onto which the graph data will be mapped
+ */
+public interface GraphResultToObjectMapper<T> {
+
+    /**
+     * Maps the data representation in the given {@link Graph} onto an object of type <code>T</code>.
+     *
+     * @param graphModel The {@link Graph} containing the data to map onto the object
+     * @return An object of type <code>T</code> containing relevant data extracted from the given graph model
+     */
+    T mapToObject(Graph graphModel);
+
+}
---
+++ src/main/java/org/neo4j/ogm/metadata/MappingConfiguration.java
@@ -0,0 +1,9 @@
+package org.neo4j.ogm.metadata;
+
+/**
+ * Provides the configuration for the object-graph mapping, including how instances of particular objects should be written
+ * to and read from the graph.
+ */
+public interface MappingConfiguration {
+
+}

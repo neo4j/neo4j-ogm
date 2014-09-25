8db7064244a88ee5e0657fe4f6a974590b75b27c
atg103 <atg103@outlook.com>
2014-09-25 17:05:35 +0100

    Adding interface-level Javadoc.

--- src/main/java/org/neo4j/ogm/metadata/ObjectCreator.java
+++ src/main/java/org/neo4j/ogm/metadata/ObjectCreator.java
@@ -3,13 +3,30 @@ package org.neo4j.ogm.metadata;
 import org.graphaware.graphmodel.Edge;
 import org.graphaware.graphmodel.Vertex;
 
-/*
- * Will probably have an implementation that works on zero-arg constructors by default.
+/**
+ * Responsible for creating objects that correspond to components of a graph model.  Note that resultant instances will
+ * not be hydrated - the {@link ObjectCreator} will only instantiate the corresponding class.
  */
 public interface ObjectCreator {
 
+    /**
+     * Creates an instance of the class that is mapped to the given {@link Vertex} according to the metadata that drives
+     * this {@link ObjectCreator}.
+     *
+     * @param vertex The {@link Vertex} for which to find the corresponding class and return an instance of it
+     * @return A new instance of an object onto which the properties of the given {@link Vertex} can be mapped
+     * @throws MappingException if there is no known class that maps to the given graph component
+     */
     <T> T instantiateObjectMappedTo(Vertex vertex);
 
+    /**
+     * Creates an instance of the class that is mapped to the given {@link Edge} according to the metadata that drives
+     * this {@link ObjectCreator}.
+     *
+     * @param edge The {@link Edge} for which to find the corresponding class and return an instance of it
+     * @return A new instance of an object onto which the properties of the given {@link Edge} can be mapped
+     * @throws MappingException if there is no known class that maps to the given graph component
+     */
     <T> T instantiateObjectMappedTo(Edge edge);
 
 }

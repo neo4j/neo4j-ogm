fa264a7cb0a4730f44b3e2863e742fa0381941f9
Vince Bickers <vince@mambofish.org>
2014-11-10 10:56:14 +0000

    cql resource for satellites integration test

--- src/main/java/org/neo4j/ogm/session/Query.java
+++ src/main/java/org/neo4j/ogm/session/Query.java
@@ -5,54 +5,54 @@ import java.util.Collection;
 public interface Query {
 
     /**
-     * fetch a single object with the specified id
+     * construct a query to fetch a single object with the specified id
      * @param id the id of the object to find
      * @return a Cypher expression
      */
     String findOne(Long id);
 
     /**
-     * fetch all objects with the specified ids
+     * construct a query to fetch all objects with the specified ids
      * @param ids the ids of the objects to find
      * @return a Cypher expression
      */
     String findAll(Collection<Long> ids);
 
     /**
-     * fetch all objects
+     * construct a query to fetch all objects
      * @return a Cypher expression
      */
     String findAll();
 
     /**
-     * fetch all objects with the specified label
+     * construct a query to fetch all objects with the specified label
      * @param label the labels attached to the objects
      * @return a Cypher expression
      */
     String findByLabel(String label);
 
     /**
-     * delete an object
+     * construct a query to delete an object
      * @param id the id of the object to delete
      * @return a Cypher expression
      */
     String delete(Long id);
 
     /**
-     * delete all objects with the specified ids
+     * construct a query to delete all objects with the specified ids
      * @param ids the ids of the objects to delete
      * @return a Cypher expression
      */
     String deleteAll(Collection<Long> ids);
 
     /**
-     * purge the database
+     * construct a query to purge the database
      * @return a Cypher expression
      */
     String purge();
 
     /**
-     * delete all objects having the supplied labels
+     * construct a query to delete all objects having the supplied label
      * @param label the label for the objects to be deleted
      * @return a Cypher expression
      */

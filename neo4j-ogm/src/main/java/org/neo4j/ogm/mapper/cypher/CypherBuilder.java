6ef548f90e95c23c54ef3b10ab1977a429b4eed2
atg103 <atg103@outlook.com>
2014-11-17 17:53:18 +0000

    Adding Javadoc.

--- neo4j-ogm/src/main/java/org/neo4j/ogm/mapper/cypher/CypherBuilder.java
+++ neo4j-ogm/src/main/java/org/neo4j/ogm/mapper/cypher/CypherBuilder.java
@@ -7,15 +7,39 @@ import java.util.List;
  */
 public interface CypherBuilder {
 
-    // I'll probably make this return a RelationshipBuilder or something
+    /**
+     * Creates a relationship from the specified start node to end node with the given relationship type.
+     *
+     * @param startNode The {@link NodeBuilder} representation of the relationship start node
+     * @param relationshipType The type of relationship to create between the node
+     * @param endNode The {@link NodeBuilder} representation of the relationship end node
+     */
     void relate(NodeBuilder startNode, String relationshipType, NodeBuilder endNode);
 
+    /**
+     * Returns a {@link NodeBuilder} that represents a new node to be created in the database.
+     *
+     * @return A {@link NodeBuilder} representing a new node
+     */
     NodeBuilder newNode();
 
+    /**
+     * Returns a {@link NodeBuilder} that represents a node that already exists in the database and matches the given ID.
+     *
+     * @param existingNodeId The ID of the node in the database
+     * @return A {@link NodeBuilder} representing the ndoe in the database that corresponds to the given ID
+     */
     NodeBuilder existingNode(Long existingNodeId);
 
     /**
-     * @return A {@link List} of Cypher queries to be executed
+     * Retrieves the Cypher queries that have been built up through this {@link CypherBuilder}.
+     * <p>
+     * Please node that there is no requirement that implementations of {@link CypherBuilder} provide an idempotent or
+     * even deterministic implementation of this method.  Therefore, it's recommended to only call this method once
+     * after all query building has been completed.
+     * </p>
+     *
+     * @return A {@link List} of Cypher queries to be executed or an empty list if there aren't any, never <code>null</code>
      */
     List<ParameterisedQuery> getStatements();
 

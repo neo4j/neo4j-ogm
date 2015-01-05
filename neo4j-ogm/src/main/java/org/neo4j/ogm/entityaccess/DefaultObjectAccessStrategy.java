54f34b9394ca1f0aca1758662a53289de6317d3c
bachmanm <git@bachman.cz>
2015-01-05 18:41:31 +0000

    improved a warning message when no matching fields / setters found

--- neo4j-ogm/src/main/java/org/neo4j/ogm/entityaccess/DefaultObjectAccessStrategy.java
+++ neo4j-ogm/src/main/java/org/neo4j/ogm/entityaccess/DefaultObjectAccessStrategy.java
@@ -227,8 +227,15 @@ public class DefaultObjectAccessStrategy implements ObjectAccessStrategy {
             return methodInfos.iterator().next();
         }
 
-        logger.warn("Cannot map iterable of {} to instance of {}.  More than one potential matching setter found.",
-                parameterType, classInfo.name());
+        if (methodInfos.size() == 0) {
+            logger.warn("Cannot map iterable of {} to instance of {}.  No matching setter found.",
+                    parameterType, classInfo.name());
+        }
+        else {
+            logger.warn("Cannot map iterable of {} to instance of {}.  More than one potential matching setter found.",
+                    parameterType, classInfo.name());
+        }
+
         return null;
     }
 
@@ -238,8 +245,15 @@ public class DefaultObjectAccessStrategy implements ObjectAccessStrategy {
             return fieldInfos.iterator().next();
         }
 
-        logger.warn("Cannot map iterable of {} to instance of {}.  More than one potential matching field found.",
-                parameterType, classInfo.name());
+        if (fieldInfos.size() == 0) {
+            logger.warn("Cannot map iterable of {} to instance of {}.  No matching field found.",
+                    parameterType, classInfo.name());
+        }
+        else {
+            logger.warn("Cannot map iterable of {} to instance of {}.  More than one potential matching field found.",
+                    parameterType, classInfo.name());
+        }
+
         return null;
     }
 

725d9bdc7c474edbf43cd2e8d11567d9eb3dcf76
Adam George <atg103@outlook.com>
2015-03-09 12:31:19 +0000

    Moving test to integration package now that defects are fixed.

--- neo4j-ogm/src/test/java/org/neo4j/ogm/integration/SessionCypherQueryTest.java
+++ neo4j-ogm/src/test/java/org/neo4j/ogm/integration/SessionCypherQueryTest.java
@@ -1,4 +1,4 @@
-package org.neo4j.ogm.defects;
+package org.neo4j.ogm.integration;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -7,7 +7,6 @@ import static org.junit.Assert.assertTrue;
 import java.util.Collections;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.neo4j.ogm.domain.cineasts.annotated.Actor;
 import org.neo4j.ogm.session.Session;
@@ -24,34 +23,24 @@ public class SessionCypherQueryTest extends WrappingServerIntegrationTest {
         session = sessionFactory.openSession(baseNeoUrl());
     }
 
-    /**
-     * NB: A similar test for Neo4jTemplate is present in Neo4jTemplateTest and should be reactivated once this is fixed.
-     */
     @Test
     public void shouldQueryForSpecificObjectUsingBespokeParameterisedCypherQuery() {
         this.session.save(new Actor("Alec Baldwin"));
         this.session.save(new Actor("Helen Mirren"));
         this.session.save(new Actor("Matt Damon"));
 
-//        setUpSession(); // including this makes the test pass
-        // FIXME: this loads any actor at random despite Neo4j always returning the correct one in t'JSON!
         Actor loadedActor = this.session.queryForObject(Actor.class, "MATCH (a:Actor) WHERE a.name={param} RETURN a",
                 Collections.singletonMap("param", "Alec Baldwin"));
         assertNotNull("The entity wasn't loaded", loadedActor);
         assertEquals("Alec Baldwin", loadedActor.getName());
     }
 
-    /**
-     * NB: A similar test for Neo4jTemplate is present in Neo4jTemplateTest and should be reactivated once this is fixed.
-     */
     @Test
     public void shouldQueryForObjectCollectionUsingBespokeCypherQuery() {
         this.session.save(new Actor("Jeff"));
         this.session.save(new Actor("John"));
         this.session.save(new Actor("Colin"));
 
-//        setUpSession(); // including this makes the test pass
-        // FIXME: this keeps loading Colin!
         Iterable<Actor> actors = this.session.query(Actor.class, "MATCH (a:Actor) WHERE a.name=~'J.*' RETURN a",
                 Collections.<String, Object>emptyMap());
         assertNotNull("The entities weren't loaded", actors);

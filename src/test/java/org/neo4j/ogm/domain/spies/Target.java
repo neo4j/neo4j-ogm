62f63ccbe1df77d22cf5a0cef4b7c9633626a3b6
Luanne Misquitta <luanne.coutinho@gmail.com>
2015-08-06 19:41:08 +0530

    DATAGRAPH-728 - Added a test to show that this works as expected.

---
+++ src/test/java/org/neo4j/ogm/domain/spies/Target.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2002-2015 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This product is licensed to you under the Apache License, Version 2.0 (the "License").
+ * You may not use this product except in compliance with the License.
+ *
+ * This product may include a number of subcomponents with
+ * separate copyright notices and license terms. Your use of the source
+ * code for these subcomponents is subject to the terms and
+ * conditions of the subcomponent's license, as noted in the LICENSE file.
+ *
+ */
+
+package org.neo4j.ogm.domain.spies;
+
+import org.neo4j.ogm.annotation.EndNode;
+import org.neo4j.ogm.annotation.RelationshipEntity;
+import org.neo4j.ogm.annotation.StartNode;
+
+/**
+ * @author Luanne Misquitta
+ */
+
+@RelationshipEntity(type = "SPY")
+public class Target {
+
+	private Long id;
+	@StartNode Spy spy;
+	@EndNode Spy target;
+	String code;
+
+	public Target() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Spy getSpy() {
+		return spy;
+	}
+
+	public void setSpy(Spy spy) {
+		this.spy = spy;
+	}
+
+	public Spy getTarget() {
+		return target;
+	}
+
+	public void setTarget(Spy target) {
+		this.target = target;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+}

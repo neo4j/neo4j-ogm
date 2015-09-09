de1216e46ae4c8c202839dd0c55be94f5b19318e
Luanne Misquitta <luanne.coutinho@gmail.com>
2015-09-09 15:12:51 +0530

    Test for issue #40

---
+++ src/test/java/org/neo4j/ogm/domain/pets/Animal.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2002-2015 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This product is licensed to you under the Apache License, Version 2.0 (the "License").
+ * You may not use this product except in compliance with the License.
+ *
+ * This product may include a number of subcomponents with
+ * separate copyright notices and license terms. Your use of the source
+ * code for these subcomponents is subject to the terms and
+ * conditions of the subcomponent's license, as noted in the LICENSE file.
+ *
+ */
+
+package org.neo4j.ogm.domain.pets;
+
+import org.neo4j.ogm.annotation.Relationship;
+
+/**
+ * @author Luanne Misquitta
+ */
+public class Animal {
+
+	private Long id;
+	private String name;
+
+	@Relationship(type = "HAS_PET", direction = "INCOMING")
+	private Kid owner;
+
+	public Animal() {
+	}
+
+	public Animal(String name) {
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Relationship(type = "HAS_PET", direction = "INCOMING")
+	public Kid getOwner() {
+		return owner;
+	}
+
+	@Relationship(type = "HAS_PET", direction = "INCOMING")
+	public void setOwner(Kid owner) {
+		this.owner = owner;
+	}
+}

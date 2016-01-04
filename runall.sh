e23b077360defccb2cc79cc8eabfdf541daf28fa
Vince Bickers <vince@mambofish.org>
2016-01-04 12:15:07 +0000

    JUnitCore test runner script now run tests for both embedded and http profiles, easily extensible to bolt when it is ready

--- runall.sh
+++ runall.sh
@@ -1,17 +1,29 @@
 #!/usr/bin/env bash
+
+# build and package with dependencies. This is done once only
 mvn clean package -DskipTests=true
 mvn dependency:copy-dependencies
 
+# set up the classpath
 INSTALLED_LIBS="../../api/target/*:../../compiler/target/*:../../core/target/*:../../drivers/target/*:../../test/target/*"
+
+# set up the configurations. All tests will run for each of these configurations
+CONFIGURATIONS=(
+    "ogm-http.properties"
+    "ogm-embedded.properties"
+)
 
 function run-tests {
     pushd $1/target
-    find  test-classes -name *Test.class | sed -e "s/\.class//" -e "s/\//./g" -e "s/test-classes\.//" | xargs java -cp $INSTALLED_LIBS:./dependency/* org.junit.runner.JUnitCore
+    find  test-classes -name *Test.class | sed -e "s/\.class//" -e "s/\//./g" -e "s/test-classes\.//" | xargs java -Dogm.properties="$CONFIGURATION" -cp $INSTALLED_LIBS:./dependency/* org.junit.runner.JUnitCore
     popd
 }
 
-run-tests "api"
-#run-tests "compiler"
-run-tests "drivers"
-#run-tests "test"
-run-tests "core"
+for CONFIGURATION in "${CONFIGURATIONS[@]}"
+do
+    echo "Running tests using configuration: $CONFIGURATION"
+    #read -n 1 -s  # uncomment this if you want to wait for user input
+    run-tests "api"
+    run-tests "drivers"
+    run-tests "core"
+done

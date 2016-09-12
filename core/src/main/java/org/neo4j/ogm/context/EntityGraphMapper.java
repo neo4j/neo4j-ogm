7059b34cdeabca9139fc43657901b2310163b611
Jasper Blues <jasper@graphaware.com>
2016-09-12 16:47:59 +0800

    Fix merge issue.

--- core/src/main/java/org/neo4j/ogm/context/EntityGraphMapper.java
+++ core/src/main/java/org/neo4j/ogm/context/EntityGraphMapper.java
@@ -235,6 +235,7 @@ public class EntityGraphMapper implements EntityMapper {
         return nodeBuilder;
     }
 
+
     /**
      * Creates a new node or updates an existing one in the graph, if it has changed.
      *
@@ -243,16 +244,18 @@ public class EntityGraphMapper implements EntityMapper {
      * @param nodeBuilder a {@link NodeBuilder} that knows how to compile node create/update cypher phrases
      */
     private void updateNode(Object entity, CompileContext context, NodeBuilder nodeBuilder) {
-
         // fire pre-save event here
-
         if (mappingContext.isDirty(entity)) {
             logger.debug("{} has changed", entity);
             context.register(entity);
             ClassInfo classInfo = metaData.classInfo(entity);
-            for (PropertyReader propertyReader : EntityAccessManager.getPropertyReaders(classInfo)) {
-                Object value = propertyReader.readProperty(entity);
-                nodeBuilder.addProperty(propertyReader.propertyName(), value);
+            Collection<PropertyReader> propertyReaders = EntityAccessManager.getPropertyReaders(classInfo);
+            for (PropertyReader propertyReader : propertyReaders) {
+                if (propertyReader.isComposite()) {
+                    nodeBuilder.addProperties(propertyReader.readComposite(entity));
+                } else {
+                    nodeBuilder.addProperty(propertyReader.propertyName(), propertyReader.readProperty(entity));
+                }
             }
         } else {
             context.deregister(nodeBuilder);

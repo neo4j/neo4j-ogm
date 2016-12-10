5e7341d083986aa75fd43abe077e23af157639f9
Mark Angrish <mark.angrish@gmail.com>
2016-12-11 02:45:20 +1100

    Initial commit to fix commit/rollback X-WRITE headers not being sent to correct node. (#300)

--- http-driver/src/main/java/org/neo4j/ogm/drivers/http/transaction/HttpTransaction.java
+++ http-driver/src/main/java/org/neo4j/ogm/drivers/http/transaction/HttpTransaction.java
@@ -48,6 +48,7 @@ public class HttpTransaction extends AbstractTransaction {
         try {
             if (transactionManager.canRollback()) {
                 HttpDelete request = new HttpDelete(url);
+                request.setHeader(new BasicHeader("X-WRITE", driver.readOnly() ? "0" : "1"));
                 driver.executeHttpRequest(request);
             }
         }
@@ -66,6 +67,7 @@ public class HttpTransaction extends AbstractTransaction {
             if (transactionManager.canCommit()) {
                 HttpPost request = new HttpPost(url + "/commit");
                 request.setHeader(new BasicHeader(HTTP.CONTENT_TYPE,"application/json;charset=UTF-8"));
+                request.setHeader(new BasicHeader("X-WRITE", driver.readOnly() ? "0" : "1"));
                 driver.executeHttpRequest(request);
             }
         }

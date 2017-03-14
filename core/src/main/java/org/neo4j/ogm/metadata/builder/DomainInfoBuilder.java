db08384bf2a0a9fa5a2970b0674684173983c1b2
Mark Angrish <mark.angrish@gmail.com>
2017-03-14 23:19:25 +1100

    Added classpath scanner test back. Improved configuration for loading from mutliple classpath types.

--- core/src/main/java/org/neo4j/ogm/metadata/builder/DomainInfoBuilder.java
+++ core/src/main/java/org/neo4j/ogm/metadata/builder/DomainInfoBuilder.java
@@ -1,11 +1,18 @@
 package org.neo4j.ogm.metadata.builder;
 
+import java.net.URL;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Set;
 
 import org.neo4j.ogm.metadata.ClassInfo;
 import org.neo4j.ogm.metadata.DomainInfo;
 import org.reflections.Reflections;
 import org.reflections.scanners.SubTypesScanner;
+import org.reflections.scanners.TypeAnnotationsScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
+import org.reflections.util.FilterBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,7 +26,16 @@ public class DomainInfoBuilder {
 
     public static DomainInfo create(String... packages) {
         // https://github.com/ronmamo/reflections/issues/80
-        Reflections reflections = new Reflections(packages, new SubTypesScanner(false));
+        final Collection<URL> urls = new HashSet<>();
+        for (String pkg : packages) {
+            urls.addAll(ClasspathHelper.forPackage(pkg, ClasspathHelper.contextClassLoader(), ClasspathHelper.staticClassLoader()));
+        }
+
+        Reflections reflections =
+                new Reflections(new ConfigurationBuilder()
+                        .setUrls(urls)
+                        .filterInputsBy(new FilterBuilder().includePackage(packages))
+                        .setScanners(new SubTypesScanner(false), new TypeAnnotationsScanner()).useParallelExecutor());
 
         DomainInfo domainInfo = new DomainInfo();
 

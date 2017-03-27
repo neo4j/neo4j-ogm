f4f8edb759cbf3ee1dff4d0bfb1cb4ed123cda7c
Nicolas Mervaillie <nmervaillie@gmail.com>
2017-03-27 14:09:25 +0200

    revert last commit

--- test/src/main/java/org/neo4j/ogm/testutil/MultiDriverTestClass.java
+++ test/src/main/java/org/neo4j/ogm/testutil/MultiDriverTestClass.java
@@ -17,6 +17,7 @@ import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.neo4j.graphdb.GraphDatabaseService;
@@ -82,12 +83,12 @@ public class MultiDriverTestClass {
 	}
 
 
-//	@After
-//	public final void tearDown() {
-//		if (impermanentDb != null) {
-//			DriverManager.deregister(DriverManager.getDriver());
-//		}
-//	}
+	@After
+	public final void tearDown() {
+		if (impermanentDb != null) {
+			DriverManager.deregister(DriverManager.getDriver());
+		}
+	}
 
 
 	public static GraphDatabaseService getGraphDatabaseService() {

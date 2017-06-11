a344de3a6c0b6ad01abaa6e592d71f6dcbfc93d4
Frantisek Hartman <frant.hartm@gmail.com>
2017-06-11 13:13:43 +0100

    Remove transaction delegate
    The delegate is only delegating to txManager.

--- core/src/main/java/org/neo4j/ogm/session/Neo4jSession.java
+++ core/src/main/java/org/neo4j/ogm/session/Neo4jSession.java
@@ -69,7 +69,6 @@ public class Neo4jSession implements Session {
     private final SaveDelegate saveDelegate = new SaveDelegate(this);
     private final DeleteDelegate deleteDelegate = new DeleteDelegate(this);
     private final ExecuteQueriesDelegate executeQueriesDelegate = new ExecuteQueriesDelegate(this);
-    private final TransactionsDelegate transactionsDelegate = new TransactionsDelegate(this);
     private final GraphIdDelegate graphIdDelegate = new GraphIdDelegate(this);
 
     private Driver driver;
@@ -455,23 +454,23 @@ public class Neo4jSession implements Session {
     */
     @Override
     public Transaction beginTransaction() {
-        return transactionsDelegate.beginTransaction();
+        return txManager.openTransaction();
     }
 
     @Override
     public Transaction beginTransaction(Transaction.Type type) {
-        return transactionsDelegate.beginTransaction(type);
+        return txManager.openTransaction(type);
     }
 
     @Override
     @Deprecated
     public <T> T doInTransaction(GraphCallback<T> graphCallback) {
-        return transactionsDelegate.doInTransaction(graphCallback);
+        return graphCallback.apply(requestHandler(), getTransaction(), metaData);
     }
 
     @Override
     public Transaction getTransaction() {
-        return transactionsDelegate.getTransaction();
+        return txManager.getCurrentTransaction();
     }
 
     /*

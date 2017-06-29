f2d7f2bcfd65753d9d0c52ebe2150f5fdce4ae24
Frantisek Hartman <frant.hartm@gmail.com>
2017-06-29 17:00:05 +0100

    Create LoadClauseBuilder based on Schema

---
+++ core/src/main/java/org/neo4j/ogm/session/request/strategy/impl/SchemaLoadClauseBuilder.java
@@ -0,0 +1,163 @@
+/*
+ * Copyright (c) 2002-2017 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This product is licensed to you under the Apache License, Version 2.0 (the "License").
+ * You may not use this product except in compliance with the License.
+ *
+ * This product may include a number of subcomponents with
+ * separate copyright notices and license terms. Your use of the source
+ * code for these subcomponents is subject to the terms and
+ *  conditions of the subcomponent's license, as noted in the LICENSE file.
+ */
+
+package org.neo4j.ogm.session.request.strategy.impl;
+
+import org.neo4j.ogm.metadata.schema.Node;
+import org.neo4j.ogm.metadata.schema.Relationship;
+import org.neo4j.ogm.metadata.schema.Schema;
+import org.neo4j.ogm.session.request.strategy.LoadClauseBuilder;
+
+import java.util.Map;
+
+import static org.neo4j.ogm.annotation.Relationship.INCOMING;
+import static org.neo4j.ogm.annotation.Relationship.OUTGOING;
+
+/**
+ * @author Frantisek Hartman
+ */
+public class SchemaLoadClauseBuilder implements LoadClauseBuilder {
+
+    private final Schema schema;
+    private final boolean pretty;
+
+    public SchemaLoadClauseBuilder(Schema schema) {
+        this.schema = schema;
+        this.pretty = false;
+    }
+
+    public String build(String variable, String label, int depth) {
+        if (depth < 0) {
+            throw new IllegalArgumentException("Only queries with depth >= 0 can be built, depth=" + depth);
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        newLine(sb);
+
+        sb.append(" RETURN ");
+        newLine(sb);
+        sb.append("n");
+        newLine(sb);
+
+        if (depth > 0) {
+            Node node = schema.findNode(label);
+            if (node.relationships().size() > 0) {
+                sb.append(",[ ");
+
+            }
+            expand(sb, variable, node, 1, depth - 1);
+            if (node.relationships().size() > 0) {
+                sb.append(" ]");
+            }
+        }
+
+
+        return sb.toString();
+    }
+
+    private void expand(StringBuilder sb, String fromNodeVar, Node node, int level, int depth) {
+        for (Map.Entry<String, Relationship> entry : node.relationships().entrySet()) {
+            if (needsSeparator(sb)) {
+                sb.append(", ");
+            }
+
+//            sb.append(entry.getKey() + " : ");
+
+            listComprehension(sb, fromNodeVar, entry.getValue(), node, level, depth);
+
+
+        }
+
+    }
+
+    private boolean needsSeparator(StringBuilder sb) {
+        for (int i = sb.length() -1; i >= 0; i--) {
+            char ch = sb.charAt(i);
+            if (!Character.isWhitespace(ch)) {
+                return ch != '[' && ch != ',';
+            }
+        }
+        return false;
+    }
+
+    private void listComprehension(StringBuilder sb, String fromNodeVar, Relationship relationship, Node node,
+                                   int level, int depth) {
+
+        String direction = relationship.direction(node);
+        Node toNode = relationship.other(node);
+
+        String relVar = relVariableName(relationship, level);
+        String toNodeVar = variableName(toNode, level);
+
+        sb.append("[ (");
+        sb.append(fromNodeVar);
+        sb.append(")");
+        switch (direction) {
+            case INCOMING:
+                appendRel(sb, relVar, relationship.type(), "<-[", "]-");
+                break;
+
+            case OUTGOING:
+                appendRel(sb, relVar, relationship.type(), "-[", "]->");
+                break;
+
+            default:
+                appendRel(sb, relVar, relationship.type(), "-[", "]-");
+                break;
+        }
+
+
+        sb.append("(");
+        sb.append(toNodeVar);
+        sb.append(":`");
+        sb.append(toNode.label());
+        sb.append("`) | [ ");
+//        sb.append(") | { rel: ");
+        sb.append(relVar);
+        sb.append(", ");
+//        sb.append(", node: ");
+        sb.append(toNodeVar);
+
+        if (depth > 0 && !toNode.relationships().isEmpty()) {
+            sb.append(", [ ");
+            expand(sb, toNodeVar, toNode, level + 1, depth - 1);
+            sb.append(" ]");
+        }
+
+        sb.append(" ] ]");
+    }
+
+    private String relVariableName(Relationship relationship, int level) {
+        return "r_" + Character.toLowerCase(relationship.type().charAt(0)) + level;
+    }
+
+    private String variableName(Node node, int level) {
+        return "" + Character.toLowerCase(node.label().charAt(0)) + level;
+    }
+
+    private void appendRel(StringBuilder sb, String variable, String type, String start, String end) {
+        sb.append(start)
+                .append(variable)
+                .append(":`")
+                .append(type)
+                .append("`")
+                .append(end);
+    }
+
+    private void newLine(StringBuilder sb) {
+        if (pretty) {
+            sb.append("\n");
+        }
+    }
+}
---
+++ core/src/test/java/org/neo4j/ogm/session/request/strategy/impl/SchemaLoadClauseBuilderTest.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2002-2017 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This product is licensed to you under the Apache License, Version 2.0 (the "License").
+ * You may not use this product except in compliance with the License.
+ *
+ * This product may include a number of subcomponents with
+ * separate copyright notices and license terms. Your use of the source
+ * code for these subcomponents is subject to the terms and
+ *  conditions of the subcomponent's license, as noted in the LICENSE file.
+ */
+
+package org.neo4j.ogm.session.request.strategy.impl;
+
+import org.junit.Test;
+import org.neo4j.ogm.metadata.DomainInfo;
+import org.neo4j.ogm.metadata.schema.DomainInfoSchemaBuilder;
+import org.neo4j.ogm.metadata.schema.Schema;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Frantisek Hartman
+ */
+public class SchemaLoadClauseBuilderTest {
+
+    @Test
+    public void buildQuery() throws Exception {
+        SchemaLoadClauseBuilder queryBuilder = createQueryBuilder();
+
+        String query = queryBuilder.build("n", "Person", 0);
+
+        assertThat(query).isEqualTo(" RETURN n");
+    }
+
+    @Test
+    public void buildQueryFromLocation() throws Exception {
+        SchemaLoadClauseBuilder queryBuilder = createQueryBuilder();
+
+        String query = queryBuilder.build("n", "Location", 1);
+        assertThat(query).isEqualTo(" RETURN n,[ [ (n)<-[r_l1:`LIVES_AT`]-(p1:`Person`) | [ r_l1, p1 ] ] ]");
+    }
+
+    @Test
+    public void buildQueryFromPerson() throws Exception {
+        SchemaLoadClauseBuilder queryBuilder = createQueryBuilder();
+
+        String query = queryBuilder.build("n", "Person", 2);
+        assertThat(query).isEqualTo(" RETURN n,[ " +
+                "[ (n)-[r_f1:`FOUNDED`]->(o1:`Organisation`) | [ r_f1, o1 ] ], " +
+                "[ (n)-[r_e1:`EMPLOYED_BY`]->(o1:`Organisation`) | [ r_e1, o1 ] ], " +
+                "[ (n)-[r_l1:`LIVES_AT`]->(l1:`Location`) | [ r_l1, l1, " +
+                "[ [ (l1)<-[r_l2:`LIVES_AT`]-(p2:`Person`) | [ r_l2, p2 ] ] ] " +
+                "] ] " +
+                "]");
+    }
+
+    @Test
+    public void givenNodeWithNoRelationships_thenCreateSimpleQuery() throws Exception {
+        SchemaLoadClauseBuilder queryBuilder = createQueryBuilder();
+
+        String query = queryBuilder.build("n", "Organisation", 1);
+
+        assertThat(query).isEqualTo(" RETURN n");
+    }
+
+    @Test
+    public void givenLabelStartingWithR_thenNodeNameAndrelationshipNameShouldNotConflict() throws Exception {
+        SchemaLoadClauseBuilder queryBuilder = createQueryBuilder();
+
+        String query = queryBuilder.build("n", "Restaurant", 1);
+
+        assertThat(query).isEqualTo(" RETURN n,[ [ (n)-[r_s1:`SIMILAR_TO`]->(r1:`Restaurant`) | [ r_s1, r1 ] ] ]");
+    }
+
+    private SchemaLoadClauseBuilder createQueryBuilder() {
+        DomainInfo domainInfo = DomainInfo.create("org.neo4j.ogm.metadata.schema.simple");
+        Schema schema = new DomainInfoSchemaBuilder(domainInfo).build();
+        return new SchemaLoadClauseBuilder(schema);
+    }
+
+}

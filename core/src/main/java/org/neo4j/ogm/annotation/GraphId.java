327ef0d6bc0da06cf55b318f591f75d75ce8f82b
Gerrit Meier <meistermeier@gmail.com>
2017-10-20 09:04:02 +0200

    Deprecate GraphId annotation. (#426)
    Mark GraphId as deprecated and advise to use the Id annotation. 
    
    Also apply import formatter.
    Fixes #417

--- core/src/main/java/org/neo4j/ogm/annotation/GraphId.java
+++ core/src/main/java/org/neo4j/ogm/annotation/GraphId.java
@@ -13,7 +13,11 @@
 
 package org.neo4j.ogm.annotation;
 
-import java.lang.annotation.*;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * Identifies the field in the domain entity which is to be
@@ -22,7 +26,10 @@ import java.lang.annotation.*;
  * field called id.
  *
  * @author Vince Bickers
+ * @deprecated Just replace {@link GraphId} with {@link Id} and {@link GeneratedValue}.
+ * By default the {@link org.neo4j.ogm.id.InternalIdStrategy} will be used.
  */
+@Deprecated
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 @Inherited

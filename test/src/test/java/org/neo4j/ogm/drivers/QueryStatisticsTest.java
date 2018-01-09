261ce46363ef150de1027802d28dcf52dadc9191
Frantisek Hartman <frant.hartm@gmail.com>
2018-01-09 11:00:19 +0100

    Report QueryStatistics correctly
    Fixes #449

---
+++ test/src/test/java/org/neo4j/ogm/drivers/QueryStatisticsTest.java
@@ -0,0 +1,193 @@
+package org.neo4j.ogm.drivers;
+
+import static java.util.Collections.*;
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.neo4j.ogm.model.QueryStatistics;
+import org.neo4j.ogm.model.Result;
+import org.neo4j.ogm.session.Session;
+import org.neo4j.ogm.session.SessionFactory;
+import org.neo4j.ogm.testutil.MultiDriverTestClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Frantisek Hartman
+ */
+public class QueryStatisticsTest extends MultiDriverTestClass {
+
+    private static final Logger logger = LoggerFactory.getLogger(QueryStatisticsTest.class);
+
+    private Session session;
+
+    @BeforeClass
+    public static void oneTimeSetUp() {
+        sessionFactory = new SessionFactory(driver, "org.neo4j.ogm.domain.social");
+    }
+
+    @Before
+    public void init() {
+        session = sessionFactory.openSession();
+        session.purgeDatabase();
+    }
+
+    @Test
+    public void statisticsContainsUpdates() {
+        Result result = session.query("CREATE (n:Node)", emptyMap());
+        QueryStatistics statistics = result.queryStatistics();
+
+        assertThat(statistics.containsUpdates()).isTrue();
+    }
+
+    @Test
+    public void statisticsNodesCreated() {
+        Result result = session.query("CREATE (n:Node)", emptyMap());
+        QueryStatistics statistics = result.queryStatistics();
+
+        assertThat(statistics.getNodesCreated()).isEqualTo(1);
+    }
+
+    @Test
+    public void statisticsNodesDeleted() {
+        session.query("CREATE (n:Node)", emptyMap());
+
+        Result result = session.query("MATCH (n:Node) DELETE n", emptyMap());
+        QueryStatistics statistics = result.queryStatistics();
+
+        assertThat(statistics.getNodesDeleted()).isEqualTo(1);
+    }
+
+    @Test
+    public void statisticsPropertiesSet() {
+        Result result = session.query("CREATE (n:Node {name:'Frantisek'})-[r:REL {weight:1.0}]->(n2:Node)", emptyMap());
+        QueryStatistics statistics = result.queryStatistics();
+
+        assertThat(statistics.getPropertiesSet()).isEqualTo(2);
+    }
+
+    @Test
+    public void statisticsRelationshipsCreated() {
+        Result result = session.query("CREATE (n:Node)-[r:REL]->(n2:Node)", emptyMap());
+        QueryStatistics statistics = result.queryStatistics();
+
+        assertThat(statistics.getRelationshipsCreated()).isEqualTo(1);
+    }
+
+    @Test
+    public void statisticsRelationshipsDeleted() {
+        session.query("CREATE (n:Node)-[r:REL]->(n2:Node)", emptyMap());
+
+        Result result = session.query("MATCH (n:Node)-[r:REL]->(n2:Node) DELETE r", emptyMap());
+        QueryStatistics statistics = result.queryStatistics();
+
+        assertThat(statistics.getRelationshipsDeleted()).isEqualTo(1);
+    }
+
+    @Test
+    public void statisticsLabelsAdded() {
+        Result result = session.query("CREATE (n:Node)", emptyMap());
+        QueryStatistics statistics = result.queryStatistics();
+
+        assertThat(statistics.getLabelsAdded()).isEqualTo(1);
+    }
+
+    @Test
+    public void statisticsLabelsRemoved() {
+        session.query("CREATE (n:Node)", emptyMap());
+        Result result = session.query("MATCH (n:Node) REMOVE n:Node", emptyMap());
+        QueryStatistics statistics = result.queryStatistics();
+
+        assertThat(statistics.getLabelsRemoved()).isEqualTo(1);
+    }
+
+    @Test
+    public void statisticsIndexesAdded() {
+        try {
+            Result result = session.query("CREATE INDEX ON :Label(property)", emptyMap());
+            QueryStatistics statistics = result.queryStatistics();
+
+            assertThat(statistics.getIndexesAdded()).isEqualTo(1);
+        } catch (Exception e) {
+            fail("Error during test", e);
+        } finally {
+
+            // try to drop the index so other tests have clean db
+            try {
+                session.query("DROP INDEX ON :Label(property)", emptyMap());
+            } catch (Exception e1) {
+                logger.warn("Error during index/constraint cleanup", e1);
+            }
+        }
+    }
+
+    @Test
+    public void statisticsIndexesRemoved() {
+        try {
+            session.query("CREATE INDEX ON :Label(property)", emptyMap());
+
+            Result result = session.query("DROP INDEX ON :Label(property)", emptyMap());
+            QueryStatistics statistics = result.queryStatistics();
+
+            assertThat(statistics.getIndexesRemoved()).isEqualTo(1);
+        } catch (Exception e) {
+            fail("Error during test", e);
+        } finally {
+
+            // try to drop the index so other tests have clean db
+            try {
+                session.query("DROP INDEX ON :Label(property)", emptyMap());
+                // expected - normally this test will remove it, here just in case something fails
+            } catch (Exception e1) {
+                logger.warn("Error during index/constraint cleanup", e1);
+            }
+        }
+    }
+
+    @Test
+    public void statisticsConstraintsAdded() {
+        try {
+            Result result = session.query("CREATE CONSTRAINT ON (n:Node) ASSERT n.property IS UNIQUE", emptyMap());
+            QueryStatistics statistics = result.queryStatistics();
+
+            assertThat(statistics.getConstraintsAdded()).isEqualTo(1);
+        } catch (Exception e) {
+            fail("Error during test", e);
+        } finally {
+
+            // try to drop the index so other tests have clean db
+            try {
+                session.query("DROP CONSTRAINT ON (n:Node) ASSERT n.property IS UNIQUE", emptyMap());
+            } catch (Exception e1) {
+                logger.warn("Error during index/constraint cleanup", e1);
+            }
+        }
+    }
+
+    @Test
+    public void statisticsConstraintsRemoved() {
+        try {
+            session.query("CREATE CONSTRAINT ON (n:Node) ASSERT n.property IS UNIQUE", emptyMap());
+
+            Result result = session.query("DROP CONSTRAINT ON (n:Node) ASSERT n.property IS UNIQUE", emptyMap());
+            QueryStatistics statistics = result.queryStatistics();
+
+            assertThat(statistics.getConstraintsRemoved()).isEqualTo(1);
+        } catch (Exception e) {
+            fail("Error during test", e);
+        } finally {
+
+            // try to drop the index so other tests have clean db
+            try {
+                session.query("DROP CONSTRAINT ON (n:Node) ASSERT n.property IS UNIQUE", emptyMap());
+            } catch (Exception e1) {
+                // expected - normally this test will remove it, here just in case something fails
+                logger.debug("Error during index/constraint cleanup", e1);
+            }
+        }
+    }
+
+}

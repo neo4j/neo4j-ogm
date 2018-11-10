20bc5156979e3a9427a1daa55220b2dad50661e3
Michael J. Simons <michael.simons@neo4j.com>
2018-11-10 12:53:49 +0100

    Add spatial support to the Embedded driver.

---
+++ embedded-driver/src/test/java/org/neo4j/ogm/drivers/embedded/types/adapter/MappedToNativePointTest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2002-2018 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This product is licensed to you under the Apache License, Version 2.0 (the "License").
+ * You may not use this product except in compliance with the License.
+ *
+ * This product may include a number of subcomponents with
+ * separate copyright notices and license terms. Your use of the source
+ * code for these subcomponents is subject to the terms and
+ *  conditions of the subcomponent's license, as noted in the LICENSE file.
+ */
+package org.neo4j.ogm.drivers.embedded.types.adapter;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.Test;
+import org.neo4j.graphdb.spatial.Point;
+import org.neo4j.ogm.types.spatial.CartesianPoint2d;
+import org.neo4j.ogm.types.spatial.CartesianPoint3d;
+import org.neo4j.ogm.types.spatial.GeographicPoint2d;
+import org.neo4j.ogm.types.spatial.GeographicPoint3d;
+
+/**
+ * @author Michael J. Simons
+ */
+public class MappedToNativePointTest {
+    @Test
+    public void mappingShouldWork() {
+        MappedToNativePoint adapter = new MappedToNativePoint();
+
+        Point point;
+
+        point = adapter.apply(new GeographicPoint2d(10, 20));
+        assertThat(point.getCRS().getCode()).isEqualTo(4326);
+        assertThat(point.getCoordinate().getCoordinate().get(0)).isEqualTo(20.0);
+        assertThat(point.getCoordinate().getCoordinate().get(1)).isEqualTo(10.0);
+
+        point = adapter.apply(new CartesianPoint2d(10, 20));
+        assertThat(point.getCRS().getCode()).isEqualTo(7203);
+        assertThat(point.getCoordinate().getCoordinate().get(0)).isEqualTo(10.0);
+        assertThat(point.getCoordinate().getCoordinate().get(1)).isEqualTo(20.0);
+
+        point = adapter.apply(new GeographicPoint3d(10.0, 20.0, 30));
+        assertThat(point.getCRS().getCode()).isEqualTo(4979);
+        assertThat(point.getCoordinate().getCoordinate().get(0)).isEqualTo(20.0);
+        assertThat(point.getCoordinate().getCoordinate().get(1)).isEqualTo(10.0);
+        assertThat(point.getCoordinate().getCoordinate().get(2)).isEqualTo(30.0);
+
+        point = adapter.apply(new CartesianPoint3d(10.0, 20.0, 30));
+        assertThat(point.getCRS().getCode()).isEqualTo(9157);
+        assertThat(point.getCoordinate().getCoordinate().get(0)).isEqualTo(10.0);
+        assertThat(point.getCoordinate().getCoordinate().get(1)).isEqualTo(20.0);
+        assertThat(point.getCoordinate().getCoordinate().get(2)).isEqualTo(30.0);
+    }
+}

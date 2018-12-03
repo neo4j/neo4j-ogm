912e0d56de27f8722b15b198eb494f94bcf14e4b
Michael Simons <michael@simons.ac>
2018-12-03 12:36:47 +0100

    GH-131 - Make base packages configurable through properties. (#561)
    Closes #131.

--- core/src/main/java/org/neo4j/ogm/session/SessionFactory.java
+++ core/src/main/java/org/neo4j/ogm/session/SessionFactory.java
@@ -76,10 +76,10 @@ public class SessionFactory {
      * Indexes will also be checked or built unless auto index mode is set to <code>NONE</code>.
      *
      * @param configuration The baseConfiguration to use
-     * @param packages      The packages to scan for domain objects
+     * @param packages      The packages to scan for domain objects. They will be merged with the configuration.
      */
     public SessionFactory(Configuration configuration, String... packages) {
-        this(newConfiguredDriverInstance(configuration), packages);
+        this(newConfiguredDriverInstance(configuration), configuration.mergeBasePackagesWith(packages));
 
         if (configuration.getAutoIndex() != NONE) {
             runAutoIndexManager(configuration);

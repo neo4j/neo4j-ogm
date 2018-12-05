18f0d32ac774d4aaaf0925987a5eb59b318eca33
Michael Simons <michael.simons@neo4j.com>
2018-12-05 16:19:11 +0100

    GH-319 - Deprecate Neo4jException.

--- core/src/main/java/org/neo4j/ogm/session/Neo4jException.java
+++ core/src/main/java/org/neo4j/ogm/session/Neo4jException.java
@@ -4,7 +4,10 @@ package org.neo4j.ogm.session;
  * Top level Exception for Neo4j OGM.
  *
  * @author Mark Angrish
+ *
+ * @deprecated Since 3.1.6, will be removed in 3.2 Neo4j-OGM doesn't use this and there will be no replacement.
  */
+@Deprecated
 public class Neo4jException extends RuntimeException {
 
     public Neo4jException(String s) {

ea9a539b238c07acbc44b6fbf0e37767fa6070ef
Gerrit Meier <gerrit.meier@neo4j.com>
2019-01-03 14:25:35 +0100

    Unify license header with date range extended to 2019.

--- core/src/main/java/org/neo4j/ogm/metadata/reflect/GenericUtils.java
+++ core/src/main/java/org/neo4j/ogm/metadata/reflect/GenericUtils.java
@@ -1,11 +1,14 @@
 /*
- * Copyright 2002-2019 the original author or authors.
+ * Copyright (c) 2002-2019 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,7 +16,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.neo4j.ogm.metadata.reflect;
 
 import java.lang.reflect.Array;

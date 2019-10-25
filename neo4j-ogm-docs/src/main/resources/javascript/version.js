99ffdcdbd66b08031dba2401f338539f44688c54
Gerrit Meier <meistermeier@gmail.com>
2019-10-25 11:58:45 +0200

    Add link to 3.2 documentation and remove 2.0 link.

--- neo4j-ogm-docs/src/main/resources/javascript/version.js
+++ neo4j-ogm-docs/src/main/resources/javascript/version.js
@@ -5,7 +5,7 @@ window.docMeta = (function () {
   return {
     name: name,
     version: version,
-    availableDocVersions: ["2.0", "2.1", "3.0", "3.1"],
+    availableDocVersions: ["2.1", "3.0", "3.1", "3.2"],
     thisPubBaseUri: href.substring(0, href.indexOf(name) + name.length) + '/' + version,
     unversionedDocBaseUri: href.substring(0, href.indexOf(name) + name.length) + '/',
     commonDocsBaseUri: href.substring(0, href.indexOf(name) - 1)

917c146a47fe3606cdc1db393e7cfe84df129e2e
Michael Simons <michael.simons@neo4j.com>
2019-11-15 18:21:39 +0100

    Polishing.

--- neo4j-ogm-tests/neo4j-ogm-integration-tests/src/test/java/org/neo4j/ogm/context/IdentityMapTest.java
+++ neo4j-ogm-tests/neo4j-ogm-integration-tests/src/test/java/org/neo4j/ogm/context/IdentityMapTest.java
@@ -73,7 +73,7 @@ public class IdentityMapTest {
         assertThat(mappingContext.isDirty(teacher)).isFalse();
     }
 
-    @Test
+    @Test // GH-684
     public void testNodeAndRelationshipWithSameId() {
 
         // Create a Node and set the Id.

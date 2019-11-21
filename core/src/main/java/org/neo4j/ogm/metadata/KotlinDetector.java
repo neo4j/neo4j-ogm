47aa573191fdbef11bcd2188354cc6cca508bd20
Michael Simons <michael.simons@neo4j.com>
2019-11-21 21:15:31 +0100

    GH-696 - Fix resolving of type descriptor in Kotlin collections.
    The Kotlin compiler uses an upper bounded wildcard type for Kotlin based node entities having Kotlin collections of Java based node entities. The previous resolving algorithm resolves this as an object, because it doesn’t do a check for wildcard types.
    
    We only add support for wildcards (for now) for usage in Kotlin, as there are explicit tests in place checking for `java.lang.Object` as type descriptor and check wether the containing type is a Kotlin class.
    
    This fixes #696.

---
+++ core/src/main/java/org/neo4j/ogm/metadata/KotlinDetector.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2002-2019 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.neo4j.ogm.metadata;
+
+import java.lang.annotation.Annotation;
+
+/**
+ * A common delegate for detecting Kotlin's presence and for identifying Kotlin types.
+ * This version has been adapted from Spring Framework's {@code org.springframework.core.KotlinDetector}. Thanks to
+ * Juergen and Sebastien.
+ *
+ * @author Juergen Hoeller
+ * @author Sebastien Deleuze
+ * @author Michael J. Simons
+ */
+@SuppressWarnings("unchecked")
+public final class KotlinDetector {
+
+    private static final Class<? extends Annotation> kotlinMetadata;
+
+    static {
+        Class<?> metadata;
+        ClassLoader classLoader = KotlinDetector.class.getClassLoader();
+        try {
+            metadata = Class.forName("kotlin.Metadata", false, classLoader);
+        } catch (ClassNotFoundException ex) {
+            // Kotlin API not available - no Kotlin support
+            metadata = null;
+        }
+        kotlinMetadata = (Class<? extends Annotation>) metadata;
+    }
+
+    /**
+     * Determine whether Kotlin is present in general.
+     */
+    public static boolean isKotlinPresent() {
+        return (kotlinMetadata != null);
+    }
+
+    /**
+     * Determine whether the given {@code Class} is a Kotlin type
+     * (with Kotlin metadata present on it).
+     */
+    public static boolean isKotlinType(Class<?> clazz) {
+        return (kotlinMetadata != null && clazz.getDeclaredAnnotation(kotlinMetadata) != null);
+    }
+
+    private KotlinDetector() {
+    }
+}
--- neo4j-ogm-tests/neo4j-ogm-integration-tests/src/test/kotlin/org/neo4j/ogm/kotlin/KotlinInteropTest.kt
+++ neo4j-ogm-tests/neo4j-ogm-integration-tests/src/test/kotlin/org/neo4j/ogm/kotlin/KotlinInteropTest.kt
@@ -16,7 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.neo4j.ogm.domain.kotlin
+package org.neo4j.ogm.kotlin
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.*

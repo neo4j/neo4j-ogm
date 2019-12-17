15582b41d8bb6e219ad985075fb12477f588b596
Michael Simons <michael.simons@neo4j.com>
2019-12-17 14:54:10 +0100

    GH-718 - Recursively traverse variable length relationship patterns.
    This change fixes the inconsistent behavior of the `org.neo4j.ogm.result.adapter.RestModelAdapter` that didn’t traverse nested things as the `org.neo4j.ogm.result.adapter.GraphModelAdapter` does. Would a fixed length pattern had been used in #718 the mapping would have worked as expected.
    
    However, it is actual the point to collect all of the relationship entities of the given point, so we have to traverse the collection of relationships we get back.
    
    This fixes #718.

--- neo4j-ogm-tests/neo4j-ogm-integration-tests/src/test/java/org/neo4j/ogm/persistence/model/RelationshipEntityMappingTest.java
+++ neo4j-ogm-tests/neo4j-ogm-integration-tests/src/test/java/org/neo4j/ogm/persistence/model/RelationshipEntityMappingTest.java
@@ -63,12 +63,6 @@ public class RelationshipEntityMappingTest extends TestContainersTestBase {
     }
 
     @Test
-    public void f() {
-
-
-    }
-
-    @Test
     public void testThatAnnotatedRelationshipOnRelationshipEntityCreatesTheCorrectRelationshipTypeInTheGraph() {
         Movie hp = new Movie("Goblet of Fire", 2005);
 

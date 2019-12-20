4eff86d447b69c7f4451150854ef6ac49778e5a0
Gerrit Meier <meistermeier@gmail.com>
2019-12-20 16:13:03 +0100

    GH-726 - Don't rely on simple class names.
    With this change it is possible to have multiple classes
    with the same simple class name in the application's domain model.
    
    In cases when custom queries were created this failed because
    of a unnecessary guard.
    
    This fixes GH-726.

--- core/src/main/java/org/neo4j/ogm/metadata/ClassInfo.java
+++ core/src/main/java/org/neo4j/ogm/metadata/ClassInfo.java
@@ -198,7 +198,7 @@ public class ClassInfo {
         return deriveSimpleName(this.cls);
     }
 
-    public static String deriveSimpleName(Class<?> clazz) {
+    private static String deriveSimpleName(Class<?> clazz) {
 
         String className = clazz.getName();
         return className.substring(className.lastIndexOf('.') + 1);

b5a1062ea32fa94e6ba98f06d09346fc87a3039f
Michael Simons <michael.simons@neo4j.com>
2020-01-13 12:11:17 +0100

    GH-737 - Add tests to ensure correct mapping of pattern comprehension based queries.

---
+++ neo4j-ogm-tests/neo4j-ogm-integration-tests/src/test/java/org/neo4j/ogm/domain/gh737/Gh737Node1.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2002-2020 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.neo4j.ogm.domain.gh737;
+
+import java.util.Objects;
+
+import org.neo4j.ogm.annotation.GeneratedValue;
+import org.neo4j.ogm.annotation.Id;
+import org.neo4j.ogm.annotation.NodeEntity;
+import org.neo4j.ogm.annotation.Relationship;
+
+/**
+ * @author Michael J. Simons
+ */
+@NodeEntity
+public class Gh737Node1 {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Relationship(type = "RELATION_A")
+    private Gh737Node2 ref;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Gh737Node2 getRef() {
+        return ref;
+    }
+
+    public void setRef(Gh737Node2 ref) {
+        this.ref = ref;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Gh737Node1 myNode = (Gh737Node1) o;
+        return Objects.equals(id, myNode.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}

6ebdbbe9e056cf7affedb92160831ec0cfda285f
Michael Simons <michael.simons@neo4j.com>
2020-07-06 15:55:57 +0200

    GH-807 - Unrelate direct subclasses during mapping of references as well.

---
+++ neo4j-ogm-tests/neo4j-ogm-integration-tests/src/test/java/org/neo4j/ogm/domain/gh806/Container.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2002-2020 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.neo4j.ogm.domain.gh806;
+
+import java.util.Set;
+
+import org.neo4j.ogm.annotation.GeneratedValue;
+import org.neo4j.ogm.annotation.Id;
+import org.neo4j.ogm.annotation.NodeEntity;
+import org.neo4j.ogm.annotation.Relationship;
+
+/**
+ * @author Michael J. Simons
+ */
+@NodeEntity
+public class Container {
+
+    @Id @GeneratedValue
+    Long id;
+
+    String name;
+
+    @Relationship(type = "RELATES_TO", direction = "INCOMING")
+    Set<Element> element;
+
+    /**
+     * This is added to ensure we don't delete unrelated relationships.
+     */
+    @Relationship(type = "RELATES_TO_TOO", direction = "INCOMING")
+    Set<ConcreteElement> elementsOfAnotherRelationship;
+
+    public Container() {
+    }
+
+    public Container(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Set<Element> getElement() {
+        return element;
+    }
+
+    public void setElement(Set<Element> element) {
+        this.element = element;
+    }
+
+    public Set<ConcreteElement> getElementsOfAnotherRelationship() {
+        return elementsOfAnotherRelationship;
+    }
+
+    public void setElementsOfAnotherRelationship(
+        Set<ConcreteElement> elementsOfAnotherRelationship) {
+        this.elementsOfAnotherRelationship = elementsOfAnotherRelationship;
+    }
+}

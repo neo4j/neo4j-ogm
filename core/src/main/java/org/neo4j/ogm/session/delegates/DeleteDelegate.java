48d8c6cefbf89ff5888b2a3c06331af2648015a7
Michael Simons <michael.simons@neo4j.com>
2020-09-01 10:37:31 +0200

    Hello, Checkstyle.

--- core/src/main/java/org/neo4j/ogm/session/delegates/DeleteDelegate.java
+++ core/src/main/java/org/neo4j/ogm/session/delegates/DeleteDelegate.java
@@ -25,7 +25,6 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 
 import org.neo4j.ogm.context.MappingContext;

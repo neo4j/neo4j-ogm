13206d029baf9f0d64cf495dbf00dbc38cadc672
Michael Simons <michael.simons@neo4j.com>
2020-09-28 09:50:07 +0200

    GH-847 - Log query notifications in Bolt transport.

--- bolt-driver/src/main/java/org/neo4j/ogm/drivers/bolt/response/BoltResponse.java
+++ bolt-driver/src/main/java/org/neo4j/ogm/drivers/bolt/response/BoltResponse.java
@@ -19,10 +19,16 @@
 package org.neo4j.ogm.drivers.bolt.response;
 
 import java.util.Set;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.neo4j.driver.Record;
 import org.neo4j.driver.Result;
 import org.neo4j.driver.exceptions.ClientException;
+import org.neo4j.driver.summary.InputPosition;
+import org.neo4j.driver.summary.Notification;
+import org.neo4j.driver.summary.ResultSummary;
 import org.neo4j.ogm.exception.CypherException;
 import org.neo4j.ogm.response.Response;
 import org.slf4j.Logger;
@@ -57,7 +63,8 @@ public abstract class BoltResponse<T> implements Response {
     @Override
     public void close() {
         // Consume the rest of the result and thus closing underlying resources.
-        result.consume();
+        ResultSummary summary = result.consume();
+        process(summary);
     }
 
     @Override
@@ -70,5 +77,68 @@ public abstract class BoltResponse<T> implements Response {
             }
         }
         return new String[0];
+    }
+
+    private static final String LINE_SEPARATOR = System.lineSeparator();
+
+    /**
+     * Does some post processing on the giving result summary, especially logging all notifications
+     * and potentially query plans.
+     *
+     * @param resultSummary The result summary to process
+     * @return The same, unmodified result summary.
+     */
+    static ResultSummary process(ResultSummary resultSummary) {
+        logNotifications(resultSummary);
+        return resultSummary;
+    }
+
+    private static void logNotifications(ResultSummary resultSummary) {
+
+        if (resultSummary.notifications().isEmpty() || !LOGGER.isWarnEnabled()) {
+            return;
+        }
+
+        String query = resultSummary.query().text();
+        resultSummary.notifications()
+            .forEach(notification -> {
+                Consumer<String> log;
+                switch (notification.severity()) {
+                    case "WARNING":
+                        log = LOGGER::warn;
+                        break;
+                    case "INFORMATION":
+                        log = LOGGER::info;
+                        break;
+                    default:
+                        log = LOGGER::debug;
+                }
+                log.accept(format(notification, query));
+            });
+    }
+
+    /**
+     * Creates a formatted string for a notification issued for a given query.
+     *
+     * @param notification The notification to format
+     * @param forQuery     The query that caused the notification
+     * @return A formatted string
+     */
+    static String format(Notification notification, String forQuery) {
+
+        InputPosition position = notification.position();
+
+        StringBuilder queryHint = new StringBuilder();
+        String[] lines = forQuery.split("(\r\n|\n)");
+        for (int i = 0; i < lines.length; i++) {
+            String line = lines[i];
+            queryHint.append("\t").append(line).append(LINE_SEPARATOR);
+            if (i + 1 == position.line()) {
+                queryHint.append("\t").append(Stream.generate(() -> " ").limit(position.column() - 1)
+                    .collect(Collectors.joining())).append("^").append(System.lineSeparator());
+            }
+        }
+        return String.format("%s: %s%n%s%s", notification.code(), notification.title(), queryHint,
+            notification.description());
     }
 }

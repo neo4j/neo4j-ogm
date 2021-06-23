7bbdf7bd1c3cf4b36223772b0b6d7233fb8a7029
Michael Simons <michael.simons@neo4j.com>
2021-06-23 12:25:14 +0200

    Add a test for SDN issue #2306.

---
+++ neo4j-ogm-tests/neo4j-ogm-integration-tests/src/test/java/org/neo4j/ogm/domain/sdn2306/Node.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2002-2021 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.neo4j.ogm.domain.sdn2306;
+
+import java.util.Set;
+
+import org.neo4j.ogm.annotation.GeneratedValue;
+import org.neo4j.ogm.annotation.Id;
+import org.neo4j.ogm.annotation.Labels;
+import org.neo4j.ogm.annotation.NodeEntity;
+
+/**
+ * @author Michael J. Simons
+ */
+@NodeEntity
+public class Node extends BaseObject {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Labels protected Set<String> neo4jLabels;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Set<String> getNeo4jLabels() {
+        return neo4jLabels;
+    }
+
+    public void setNeo4jLabels(Set<String> neo4jLabels) {
+        this.neo4jLabels = neo4jLabels;
+    }
+}
--- neo4j-ogm-tests/neo4j-ogm-integration-tests/src/test/java/org/neo4j/ogm/persistence/session/capability/QueryCapabilityTest.java
+++ neo4j-ogm-tests/neo4j-ogm-integration-tests/src/test/java/org/neo4j/ogm/persistence/session/capability/QueryCapabilityTest.java
@@ -18,8 +18,10 @@
  */
 package org.neo4j.ogm.persistence.session.capability;
 
-import static java.util.Collections.*;
-import static org.assertj.core.api.Assertions.*;
+import static java.util.Collections.emptyMap;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.LoggerContext;
@@ -58,6 +60,9 @@ import org.neo4j.ogm.domain.gh875.AbstractPet;
 import org.neo4j.ogm.domain.gh875.Cat;
 import org.neo4j.ogm.domain.gh875.Kennel;
 import org.neo4j.ogm.domain.gh875.Person;
+import org.neo4j.ogm.domain.sdn2306.AbstractNodeBImplA;
+import org.neo4j.ogm.domain.sdn2306.SubNodeB;
+import org.neo4j.ogm.domain.sdn2306.SubNodeBImplA;
 import org.neo4j.ogm.domain.linkedlist.Item;
 import org.neo4j.ogm.domain.nested.NestingClass;
 import org.neo4j.ogm.domain.restaurant.Restaurant;
@@ -102,7 +107,8 @@ public class QueryCapabilityTest extends TestContainersTestBase {
             "org.neo4j.ogm.domain.linkedlist",
             "org.neo4j.ogm.domain.gh726",
             "org.neo4j.ogm.domain.gh851",
-            "org.neo4j.ogm.domain.gh875"
+            "org.neo4j.ogm.domain.gh875",
+            "org.neo4j.ogm.domain.sdn2306"
         );
         session = sessionFactory.openSession();
         session.purgeDatabase();
@@ -143,6 +149,50 @@ public class QueryCapabilityTest extends TestContainersTestBase {
         session.purgeDatabase();
     }
 
+    @Test // SDN-2306
+    public void shouldFillLabelsInComplexInheritanceHierachies() {
+
+        Long id1 = session.queryForObject(Long.class,
+            "create (a:Node:SubNodeA:SubNodeB:SubNodeBImplA {name: 'eins'}) RETURN id(a)",
+            Collections.emptyMap());
+        Long id2 = session.queryForObject(Long.class,
+            "create (a:Node:SubNodeA:SubNodeB:AbstractNodeBImplA:More:Labels {name: 'zwei'})  RETURN id(a)",
+            Collections.emptyMap());
+
+        assertThat(id1).isNotNull();
+        assertThat(id2).isNotNull();
+
+        SubNodeB b1 = session.load(SubNodeB.class, id1);
+        assertThat(b1).isNotNull();
+        assertThat(b1).isInstanceOf(SubNodeBImplA.class);
+        assertThat(b1.getName()).isEqualTo("eins");
+        assertThat(b1.getNeo4jLabels()).isEmpty();
+
+        SubNodeB b2 = session.load(SubNodeB.class, id2);
+        assertThat(b2).isNotNull();
+        assertThat(b2).isInstanceOf(AbstractNodeBImplA.class);
+        assertThat(b2.getName()).isEqualTo("zwei");
+        assertThat(b2.getNeo4jLabels()).containsExactly("More", "Labels");
+
+        b1.getNeo4jLabels().add("X");
+        b1.getNeo4jLabels().add("Y");
+        session.save(b1);
+
+        b2.getNeo4jLabels().add("New1");
+        b2.getNeo4jLabels().add("New2");
+        session.save(b2);
+
+        long cnt = session.queryForObject(Long.class,
+            "match (a:X:Y) WHERE id(a) = $id RETURN count(a)",
+            Collections.singletonMap("id", id1));
+        assertThat(cnt).isEqualTo(1L);
+
+        cnt = session.queryForObject(Long.class,
+            "match (a:New1:New2) WHERE id(a) = $id RETURN count(a)",
+            Collections.singletonMap("id", id2));
+        assertThat(cnt).isEqualTo(1L);
+    }
+
     @Test // DATAGRAPH-697
     public void shouldQueryForArbitraryDataUsingBespokeParameterisedCypherQuery() {
         session.save(new Actor("Helen Mirren"));
@@ -168,7 +218,8 @@ public class QueryCapabilityTest extends TestContainersTestBase {
     public void readOnlyQueryMustBeReadOnly() {
 
         session.save(new Actor("Jeff"));
-        if (isVersionOrGreater("4.1") && isBoltDriver()) { // 4.1+ will fail on any attempt to write in a read-only transaction.
+        if (isVersionOrGreater("4.1")
+            && isBoltDriver()) { // 4.1+ will fail on any attempt to write in a read-only transaction.
             assertThatThrownBy(() -> session.query("MATCH (a:Actor) SET a.age=$age", Utils.map("age", 5), true))
                 .hasMessageStartingWith("Writing in read access mode not allowed.");
         } else {
@@ -857,7 +908,6 @@ public class QueryCapabilityTest extends TestContainersTestBase {
             .containsExactly("A", "B", "C", "D");
     }
 
-
     @Test // GH-726
     public void shouldMapCorrectlyIfTwoClassesWithTheSameSimpleNameExist() {
         // org.neo4j.ogm.domain.gh726.package_a.SameClass
@@ -885,12 +935,13 @@ public class QueryCapabilityTest extends TestContainersTestBase {
     @Test // GH-737
     public void shouldReturnListOfNodesAndRelationshipModelForUnknownRelationshipLists() {
         Result result = session
-            .query("MATCH (n:Movie{title:'Pulp Fiction'}) return n, [(n)-[r:UNKNOWN]-(p) | [r,p]] as relAndNode", emptyMap());
+            .query("MATCH (n:Movie{title:'Pulp Fiction'}) return n, [(n)-[r:UNKNOWN]-(p) | [r,p]] as relAndNode",
+                emptyMap());
         Map<String, Object> returnedRow = result.queryResults().iterator().next();
 
         assertThat(returnedRow.get("n")).isInstanceOf(Movie.class);
-        assertThat(((List)returnedRow.get("relAndNode")).get(0)).isInstanceOf(Pet.class);
-        assertThat(((List)returnedRow.get("relAndNode")).get(1)).isInstanceOf(RelationshipModel.class);
+        assertThat(((List) returnedRow.get("relAndNode")).get(0)).isInstanceOf(Pet.class);
+        assertThat(((List) returnedRow.get("relAndNode")).get(1)).isInstanceOf(RelationshipModel.class);
     }
 
     @Test // GH-851
@@ -940,7 +991,7 @@ public class QueryCapabilityTest extends TestContainersTestBase {
     @Test // GH-875
     public void polymorphicQueryShouldIncludeAllRelTypes() {
 
-        for(int depth : new int[] {-1, 10}){
+        for (int depth : new int[] { -1, 10 }) {
             Collection<Kennel> kennels = sessionFactory.openSession().loadAll(Kennel.class, depth);
             assertThat(kennels).hasSize(2);
             assertThat(kennels).allSatisfy(kennel -> {

75d947b06d11737e69ddb6ec13f47c223cb20db7
Michael Simons <michael.simons@neo4j.com>
2021-11-24 12:43:31 +0100

    Update to the latest 4.4 driver.

--- neo4j-ogm-tests/neo4j-ogm-integration-tests/src/test/java/org/neo4j/ogm/persistence/session/capability/QueryCapabilityTest.java
+++ neo4j-ogm-tests/neo4j-ogm-integration-tests/src/test/java/org/neo4j/ogm/persistence/session/capability/QueryCapabilityTest.java
@@ -243,7 +243,7 @@ public class QueryCapabilityTest extends TestContainersTestBase {
             && isBoltDriver()) { // 4.1+ will fail on any attempt to write in a read-only transaction.
             assertThatThrownBy(
                 () -> session.query("MATCH (a:Actor) SET a.age=$age", Collections.singletonMap("age", 5), true))
-                .hasMessageStartingWith("Writing in read access mode not allowed.");
+                .hasMessageContaining("Writing in read access mode not allowed.");
         } else {
             session.query("MATCH (a:Actor) SET a.age=$age", Collections.singletonMap("age", 5), true);
 

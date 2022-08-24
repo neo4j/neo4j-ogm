5a0679487ddb8bbd987ebd94bf35cc085b11f927
Michael Simons <michael.simons@neo4j.com>
2022-08-24 20:28:03 +0200

    Make reuse of existing builders threadsafe, check for property equality.

--- core/src/main/java/org/neo4j/ogm/cypher/compiler/MultiStatementCypherCompiler.java
+++ core/src/main/java/org/neo4j/ogm/cypher/compiler/MultiStatementCypherCompiler.java
@@ -21,6 +21,7 @@ package org.neo4j.ogm.cypher.compiler;
 import static java.util.stream.Collectors.*;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -61,7 +62,7 @@ public class MultiStatementCypherCompiler implements Compiler {
         this.context = new CypherContext(this, nativeIdProvider);
         this.newNodeBuilders = new ArrayList<>();
         this.newRelationshipBuilders = new ArrayList<>();
-        this.existingNodeBuilders = new HashMap<>();
+        this.existingNodeBuilders = new ConcurrentHashMap<>();
         this.existingRelationshipBuilders = new LinkedHashMap<>(); // Order for the relationships IS important
         this.deletedRelationshipBuilders = new ArrayList<>();
         this.deletedRelationshipEntityBuilders = new ArrayList<>();
@@ -94,9 +95,12 @@ public class MultiStatementCypherCompiler implements Compiler {
     @Override
     public RelationshipBuilder existingRelationship(Long existingRelationshipId, String direction, String type, boolean wasDirty) {
         String key = existingRelationshipId + ";" + direction;
-        RelationshipBuilder relationshipBuilder = existingRelationshipBuilders.computeIfAbsent(key, k -> new DefaultRelationshipBuilder(type, existingRelationshipId));
-        relationshipBuilder.setDirty(wasDirty);
-        return relationshipBuilder;
+        synchronized (this) {
+            RelationshipBuilder relationshipBuilder = existingRelationshipBuilders.computeIfAbsent(key,
+                k -> new DefaultRelationshipBuilder(type, existingRelationshipId));
+            relationshipBuilder.setDirty(wasDirty);
+            return relationshipBuilder;
+        }
     }
 
     @Override
@@ -196,25 +200,30 @@ public class MultiStatementCypherCompiler implements Compiler {
     public List<Statement> updateRelationshipStatements() {
         assertStatementFactoryExists();
 
-        if (existingRelationshipBuilders.isEmpty()) {
-            return Collections.emptyList();
+        synchronized (this) {
+            if (existingRelationshipBuilders.isEmpty()) {
+                return Collections.emptyList();
+            }
+
+            Map<Boolean, Set<Edge>> collect = existingRelationshipBuilders.values().stream()
+                .collect(partitioningBy(RelationshipBuilder::isDirty,
+                    Collectors.mapping(RelationshipBuilder::edge, Collectors.toSet())));
+
+            List<Statement> result = new ArrayList<>();
+            if (!collect.get(true).isEmpty()) {
+                ExistingRelationshipStatementBuilder builder = new ExistingRelationshipStatementBuilder(
+                    collect.get(true), statementFactory, true);
+                result.add(builder.build());
+            }
+
+            if (!collect.get(false).isEmpty()) {
+                ExistingRelationshipStatementBuilder builder = new ExistingRelationshipStatementBuilder(
+                    collect.get(false), statementFactory, false);
+                result.add(builder.build());
+            }
+
+            return result;
         }
-
-        Map<Boolean, Set<Edge>> collect = existingRelationshipBuilders.values().stream()
-            .collect(partitioningBy(RelationshipBuilder::isDirty, Collectors.mapping(RelationshipBuilder::edge, Collectors.toSet())));
-
-        List<Statement> result = new ArrayList<>();
-        if (!collect.get(true).isEmpty()) {
-            ExistingRelationshipStatementBuilder builder = new ExistingRelationshipStatementBuilder(collect.get(true), statementFactory, true);
-            result.add(builder.build());
-        }
-
-        if (!collect.get(false).isEmpty()) {
-            ExistingRelationshipStatementBuilder builder = new ExistingRelationshipStatementBuilder(collect.get(false), statementFactory, false);
-            result.add(builder.build());
-        }
-
-        return result;
     }
 
     @Override
